4991e7586cd5a2db9909562323d5037e27613e0c
ukLz <DimitrijE02>
2019-12-07 02:49:57 +0100

    321
    123

---
+++ playingwithjs.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html> 
+<html> 
+
+<head> 
+	<title> 
+            GAY TESTER  69 
+	</title> 
+</head> 
+
+<body style="text-align: left;"> 
+	<h1 style="color:violet;"> 
+        <b>YOU  FUCKING  SUCK</b> 
+	</h1> 
+	
+    <br/>
+
+	
+    <form>
+            <input type="button" value="TAKE A SHORT QUIZ " onclick="msg()">
+    </form>
+    
+
+    <script>
+		// JS ovde.
+			
+		function checkNumber() {
+				var rc = false;
+				if (!isNaN(parseFloat(n)) && isFinite(n)) {
+					rc = true;
+				}
+				else {
+					document.getElementById("num_fld").value = 0;
+				}
+				return rc;
+			}
+
+			function msg() 
+			{
+                    var yes = "yes";
+                    
+                    
+                    //var i;
+                    //for (i = 0, i < 5, i++)
+                //{
+                alert("Hello there!");
+                let name1 = prompt("What is your name?");
+                
+                alert("Hello, " + name1 + "!");
+                let name2 = prompt("How old are you?");
+                alert("Okay, so, you're " + name2 + " years old, right?");
+                alert("Alright, cool... One more question for ya:");
+                var newVal = prompt("Are you gay?", yes);
+                if (newVal === "yes")
+                    {
+                        alert("thank you for admitting!");
+                    }
+                else
+                    {
+                        alert("yes, you are indeed gay  .i.");
+                    }
+
+			}
+			
+		//}
+		
+		
+</script>
+
+</body> 
+
+</html> 
